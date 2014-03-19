cd03992739e683630d09668be91ad97436219abf
jacklam <jacklam718@gmail.com>
2014-03-19 12:58:29 +0800

    Update tcpSocket.py

--- tcpSocket.py
+++ tcpSocket.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 """ 
-Why am I writing this program: Although python socket is very 
+Why I would writing this program: Although python socket is very 
 easy to use, but I hope there have an easier way, just call the 
 method then to use, for example does not need too much exception 
 handling.
