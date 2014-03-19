346978452bdb2a2830975a6edb4e4316ca3fb06c
jacklam <jacklam718@gmail.com>
2014-03-19 12:58:55 +0800

    Update tcpSocket.py

--- tcpSocket.py
+++ tcpSocket.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python
-""" 
-Why I would writing this program: Although python socket is very 
-easy to use, but I hope there have an easier way, just call the 
-method then to use, for example does not need too much exception 
-handling.
-"""
 from socket import * 
 from threading import Thread
 
